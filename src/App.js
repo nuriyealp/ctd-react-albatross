eb106164ba9ebe368b7555f1e2e5416ef462fa03
Nur <nuriyealp66@gmail.com>
2022-07-04 07:23:23 -0700

    ceatea toDoList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import React from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 
 function App() {
-  const [newTodo, setNewTodo] = React.useState('');
+  const [todoList, setTodoList] = React.useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
+
+  console.log(todoList);
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm  onAddTodo={setNewTodo}/>
-      <p>
-        {newTodo}
-      </p>
-      <TodoList />
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
